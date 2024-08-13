a22097144a831a531a2260ffbb657ec7d6ae1c69
Imran Mohammed <imo60067035@gmail.com>
2024-08-13 11:52:54 +0300

    create and test models

---
+++ mobile/ecommerce_app/test/features/product/data/model/product_model_test.dart
@@ -0,0 +1,46 @@
+
+import 'dart:convert';
+
+import 'package:ecommerce_app/features/product/data/models/product_model.dart';
+import 'package:ecommerce_app/features/product/domain/entities/product.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
+
+void main() {
+  const ProductModel tProductModel = ProductModel(
+    id: '1',
+    name: 'Product 1',
+    description: 'Description 1',
+    price: 100.0,
+    imageUrl: 'image1.jpg',
+  );
+
+  test('should be a subclass of Product entity', () {
+    // assert
+    expect(tProductModel, isA<Product>());
+  });
+
+  test('should convert json to a model object', () {
+    // arrange
+    final Map<String, dynamic> jsonMap = json.decode(fixtureReader('product.json'));
+
+    // act
+    final result = ProductModel.fromJson(jsonMap);
+
+    // assert
+    expect(result, tProductModel);
+  });
+
+  test('should convert product model object to a json', () {
+    // arrange
+    final Map<String, dynamic> expectedMap = json.decode(fixtureReader('product.json'));
+
+    // act
+    final result = tProductModel.toJson();
+
+    // assert
+    expect(result, expectedMap);
+  });
+  
+}
